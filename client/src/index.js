e7ecb25403629c68141e5b2857b8f275af284d26
UMAIR936 <umairghaffar936@gmail.com>
2023-10-11 15:36:15 +0330

    develop auth apis

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-import { reducers } from './reducers';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { reducers } from "./reducers";
+import App from "./App";
+import "./index.css";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
+  <GoogleOAuthProvider clientId={process.env.client_Id}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </GoogleOAuthProvider>,
+  document.getElementById("root")
 );
